b347d64cf2ab538ed3298598274fc8ee81194408
yamenka <yamenkassemm@gmail.com>
2024-03-13 11:01:07 +0000

    New Commit for releae V1.0.0

---
+++ ExpenseBudgetTracker/ExpenseBudgetTracker/Budget.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace ExpenseBudgetTracker
+{
+	public class Budget
+	{
+        public Dictionary<string, decimal> CategoryBudgets { get; set; }
+
+        public Budget()
+        {
+            CategoryBudgets = new Dictionary<string, decimal>();
+        }
+
+        public void SetBudget(string category, decimal amount)
+        {
+            if (CategoryBudgets.ContainsKey(category))
+            {
+                CategoryBudgets[category] = amount;
+            }
+            else
+            {
+                CategoryBudgets.Add(category, amount);
+            }
+        }
+
+        public decimal GetCategoryBudget(string category)
+        {
+            return CategoryBudgets.ContainsKey(category) ? CategoryBudgets[category] : 0;
+        }
+    }
+}
+
---
+++ ExpenseBudgetTracker/ExpenseBudgetTracker/Transaction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace ExpenseBudgetTracker
+{
+	public abstract class Transaction
+    {
+        public decimal Amount { get; set; }
+        
+        public string Category { get; set; }
+        public string Note { get; set; }
+        public bool IsRecurring { get; set; }
+
+        public TransactionType Type { get; set; }
+
+
+    public Transaction(decimal amount, TransactionType type, string category, string note, bool isRecurring)
+        {
+            Amount = amount;
+            Type = type;
+            Category = category;
+            Note = note;
+            IsRecurring = isRecurring;
+        }
+
+        public override string ToString()
+        {
+            return $"Amount: {Amount}, Type: {Type}, Category: {Category}, Note: {Note}, Recurring: {IsRecurring}";
+        }
+
+    }
+}
+
