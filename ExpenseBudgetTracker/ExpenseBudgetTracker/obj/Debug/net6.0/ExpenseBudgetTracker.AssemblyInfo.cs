4e039b75485d3e7d686a9beec24b9c933876080a
yamenka <yamenkassemm@gmail.com>
2024-03-13 11:17:32 +0000

    Readme Added

--- ExpenseBudgetTracker/ExpenseBudgetTracker/obj/Debug/net6.0/ExpenseBudgetTracker.AssemblyInfo.cs
+++ ExpenseBudgetTracker/ExpenseBudgetTracker/obj/Debug/net6.0/ExpenseBudgetTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseBudgetTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e529eaf6607e1d4521f22f289a5631d7cfcc8dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4e25c6a90655bd078c05aae4ac4d46220cece2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseBudgetTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseBudgetTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
